81796ee8d206a0872dd51e08b734568b17e7dcdf
judithli <judyloverairiarte@gmail.com>
2022-05-20 15:39:36 -0300

    arregloError

--- Turismo/views.py
+++ Turismo/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from Turismo.forms import ConsultaFormulario
 from Turismo.models import Consultas, Destino
 
+
 def destino(request):
 
     return render(request,"Turismo/destino.html")
@@ -34,15 +35,16 @@ def consultaFormulario(request):
         if miFormulario.is_valid():
             informacion = miFormulario.cleaned_data
 
-        consultas1 = Consultas( 
+        consultas = Consultas( 
             consulta=informacion['consultas'], 
             nombre=informacion['nombre'], 
             apellido=informacion['apellido'],
-            mail=informacion['mail'])
-        consultas1.save()
+            mail=informacion['mail'],)
+        consultas.save()
 
         return render(request, "Turismo/inicio.html")
     else:
         miFormulario = ConsultaFormulario()
+    
     
     return render(request, "Turismo/consultaFormulario.html", {"miFormulario":miFormulario})
