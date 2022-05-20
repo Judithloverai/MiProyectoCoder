961411d1bdb7aab74157fd5884e67185edbc53a4
judithli <judyloverairiarte@gmail.com>
2022-05-20 00:54:22 -0300

    Salvado

--- Turismo/models.py
+++ Turismo/models.py
@@ -15,7 +15,7 @@ class Profesionales(models.Model):
     nombre = models.CharField(max_length=40)
     apellido = models.CharField(max_length=40)
     email = models.EmailField()
-    cargo = models.CharField(max_length=40)
+    cargo = models.CharField(max_length=40, default="")
 
 
     
