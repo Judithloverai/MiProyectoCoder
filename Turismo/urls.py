2ba9b0cd8501b93316418c1f48e0e804dedc12c1
judithli <judyloverairiarte@gmail.com>
2022-05-30 22:06:22 -0300

    Arreglo Final

--- Turismo/urls.py
+++ Turismo/urls.py
@@ -18,7 +18,6 @@ urlpatterns = [
     path('consultas/lista', views.ConsultasList.as_view(), name ="ListConsultas"),
     path(r'^borrar/(?P<pk>\d+)$', views.ConsultaDelete.as_view(), name='Delete'),
     path(r'^(?P<pk>\d+)$', views.ConsultaDetalle.as_view(), name='Detail'),
-
     path('login', views.login_request, name='Login'),
     path('logout', LogoutView.as_view(template_name='Turismo/logout.html'), name='Logout'),
     path('Registrar', views.register, name='Registrar'),
